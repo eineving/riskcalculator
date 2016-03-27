55a550aa95165e7766f25dc97bbdd2fd1a49effc
Daniel Eineving <daniel@eineving.se>
2016-03-27 22:27:55 +0200

    Setup

---
+++ src/main/java/Scenario.java
@@ -0,0 +1,12 @@
+/**
+ * Created by danie on 2016-03-27.
+ */
+public class Scenario {
+    int startAttacker, startDefender;
+
+    public Scenario(int attackers, int defenders){
+        startAttacker = attackers;
+        startDefender = defenders;
+    }
+
+}
