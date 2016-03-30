6bb86d4b1347c4ff7bd87b38a6b3eca3b8eaee04
Daniel Eineving <daniel@eineving.se>
2016-03-30 21:35:21 +0200

    Made maven project instead

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import spark.Spark.*;
 /**
  * Created by Daniel on 2016-03-29.
  */
 public class Main {
 
     public static void main(String [] argdgsderrg){
-        TreeOutcome tree = new TreeOutcome(16,16);
+        TreeOutcome tree = new TreeOutcome(18,18);
         System.out.println(tree.getOutcome());
+
     }
 }
